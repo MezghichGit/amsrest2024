7dd47ac5341cb91e6c5d56bd4996d89b33e41242
moahmed amine mezghich <git@mezghich.com>
2024-02-04 11:03:02 +0100

    Partie 1 : Registration OK

---
+++ src/main/java/com/sip/ams/controllers/TestController.java
@@ -0,0 +1,35 @@
+package com.sip.ams.controllers;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/test")
+public class TestController {
+  @GetMapping("/all")
+  public String allAccess() {
+    return "Public Content.";
+  }
+
+  @GetMapping("/user")
+  @PreAuthorize("hasRole('USER') or hasRole('ADMIN') or hasRole('SUPERADMIN')")
+  public String userAccess() {
+    return "User Content.";
+  }
+
+  @GetMapping("/superadmin")
+  @PreAuthorize("hasRole('SUPERADMIN')")
+  public String superAdminAccess() {
+    return "SuperAdmin Board.";
+  }
+
+  @GetMapping("/admin")
+  @PreAuthorize("hasRole('ADMIN')")
+  public String adminAccess() {
+    return "Admin Board.";
+  }
+}
